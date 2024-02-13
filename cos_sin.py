1437b41bf4b0dc31019d139b76687685b1ad0683
sadistic001 <mouad.sadik@ig2i.centralelillle.fr>
2024-02-13 11:39:26 +0100

    connection to llama model

--- cos_sin.py
+++ cos_sin.py
@@ -1,8 +1,10 @@
+# cos_sin.py
 from scipy import spatial
 from operator import itemgetter
 
 def cos_sin(all_paragraphs, embeddings, question):
     cosine_list = []
+    reponses = []
 
     for index_sentence, xt in enumerate(embeddings):
         result = 1 - spatial.distance.cosine(question, xt)
@@ -10,8 +12,8 @@ def cos_sin(all_paragraphs, embeddings, question):
 
     cosine_list.sort(key=itemgetter('res'), reverse=True)
     for indice, row in enumerate(cosine_list):
-        print(all_paragraphs[cosine_list[indice]['i']])
-        if indice + 1 == 10:
+        reponses.append(all_paragraphs[cosine_list[indice]['i']])
+        if indice + 1 == 3:
             break
 
-    return "t'as les crampter"
+    return '\n'.join(reponses)
--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 from decoupage import main_decoupage
 from vectoriser import vectoriser
 from cos_sin import cos_sin
+import subprocess
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+chemin_executable_llama = "/Users/mouadsadik/Projects/llama.cpp/main"
+chemin_modele = "/Users/mouadsadik/Projects/llama.cpp/models/openhermes-2.5-mistral-7b.Q4_K_M.gguf"
 
 
 if __name__ == "__main__":
@@ -11,8 +18,23 @@ if __name__ == "__main__":
     all_paragraphs = main_decoupage()
     embeddings = vectoriser(all_paragraphs)
     embeded_question = vectoriser(question)
-    cos_sin(all_paragraphs, embeddings, embeded_question)
-    print(embeddings)
+    reponses = cos_sin(all_paragraphs, embeddings, embeded_question)
+    print("\n\n\n -------------------------------------------------")
+    commande = [
+        chemin_executable_llama,
+        "-m", chemin_modele,
+        "-p", ' Je vais te donner un context à partir de mes fichier que tu vas utiliser pour construire une réponse.\n Voici le contexte : ' + reponses + '.\n Voici ma question :' + question + '\n Réponse : ',
+        "-n", "1000",
+        "-e" 
+         # Active le mode évaluation
+    ]
+
+    
+
+    resultat = subprocess.run(commande, capture_output=True, text=True)
+
+    # Affichez la sortie
+    print(resultat.stdout)
     
 
 
