505901d5f21012aced05809e325564516546829a
BaptisteBRT <baptiste.bortolotti@ig2i.centralelille.fr>
2024-02-13 08:34:24 +0100

    hotfix variable name

--- vectoriser.py
+++ vectoriser.py
@@ -3,5 +3,5 @@ from sentence_transformers import SentenceTransformer
 def vectoriser(paragraphs):
 
     model = SentenceTransformer('intfloat/multilingual-e5-base')
-    embeddings = model.encode(input_texts, normalize_embeddings=True)
+    embeddings = model.encode(paragraphs, normalize_embeddings=True)
     return embeddings
